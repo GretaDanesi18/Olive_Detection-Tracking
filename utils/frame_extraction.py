66526701376e42d8eddb1b02e31d479964b339dc
GretaDanesi18 <danesi.1804735@studenti.uniroma1.it>
2022-07-14 10:43:12 +0200

    utils

--- utils/frame_extraction.py
+++ utils/frame_extraction.py
@@ -8,12 +8,13 @@ file_path_2 = "../Olive_Detection_Tracking/dataset_video/videos/video2.mp4"
 
 def frame_extraction(video_name):
     file_name = video_name.split('/')[4].split('.')[0]
-    path = '../Olive_Detection_Tracking/dataset_video/frame_'+file_name
+    path = '../Olive_Detection_Tracking/dataset_video/frame_video/frame_'+file_name
     try:
-        if not os.path.exists(path):
-            os.mkdir(path)
-        elif os.path.exists(path):
+        if os.path.exists(path):
             print("La cartella già esiste")
+            
+        elif not os.path.exists(path):
+            os.makedirs(path)
 
     except OSError:
         print("Errore nella creazione della cartella "+path)
